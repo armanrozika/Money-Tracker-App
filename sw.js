2d981741fbeb2a3d18ba5fd9100679563444e58e
armanrozika <padahariyanglain@gmail.com>
2018-10-26 21:33:54 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,33 +1,36 @@
-self.addEventListener('install', function(){
-    console.log('installed');
+self.addEventListener('install', function (event) {
+  console.log('SW Installed');
+  event.waitUntil(
     caches.open('static')
-    .then(function(cache){
+      .then(function (cache) {
         cache.addAll([
-            '/',
-            'index.html',
-            'static/js/app.50c4a9b13be5704977f6.js',
-            'static/js/manifest.2ae2e69a05c33dfc65f8.js',
-            'static/js/vendor.c67cbddf1abd9a514d69.js',
-            'static/css/app.d9ab212628a22c06daad10efbe277b1e.css',
-            'https://fonts.googleapis.com/css?family=Montserrat',
-            'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'
-        ])
-    })
+          '/',
+          'index.html',
+          'static/js/main.js',
+          'static/js/app.50c4a9b13be5704977f6.js',
+          'static/js/manifest.2ae2e69a05c33dfc65f8.js',
+          'static/js/vendor.c67cbddf1abd9a514d69.js',
+          'static/css/app.d9ab212628a22c06daad10efbe277b1e.css',
+          'https://fonts.googleapis.com/css?family=Montserrat',
+          'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css',
+        ]);
+      })
+  );
 });
 
-self.addEventListener('activated', function(){
-    console.log('activated')
+self.addEventListener('activate', function () {
+  console.log('SW Activated');
 });
 
-self.addEventListener('fetch', function(event){
-    event.respondWith(
-        caches.match(event.request)
-        .then(function(res){
-            if(res){
-                return res;
-            }else{
-                fetch(event.request);
-            }
-        })
-    )
-})
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(res) {
+        if (res) {
+          return res;
+        } else {
+          return fetch(event.request);
+        }
+      })
+  );
+});
